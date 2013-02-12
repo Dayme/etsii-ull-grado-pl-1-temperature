f9f5f6da7ce84aa329ad23edcaf13d76916b199b
Dayme <daymegr@yahoo.es>
2013-02-12 18:52:01 +0000

    modified

--- temperature.js
+++ temperature.js
@@ -3,13 +3,13 @@ function calculate() {
   var result;
   var original = document.getElementById("original");
   var temp = original.value; // leemos el valor de la etiqueta <input> 
-  var regexp = /^[-+]?(\d+(\.\d+)?)\s*([CFcf])/;
+  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
                //parentisamos la parte del no. y la del tipo de grados
   var m = temp.match(regexp); //devuelve un array con la cadena que ha sido 
                              //cazada con el exp regular en la 1ra pos.
   if (m) {   //si se ha cazado algun valor(no devolvio null)
     var num = m[1];    //nos quedamos con el numero
-    var type = m[3];   //nos quedamos con el tipo de grados
+    var type = m[2];   //nos quedamos con el tipo de grados
     num = parseFloat(num);   //convertimos la cadena que representa al no. a un Float
                              
     if (type == 'c' || type == 'C') {
